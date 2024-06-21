513cabcd15e1c39fec400ea265eeecba621e97e3
ProsperousPotato <114923153+ProsperousPotato@users.noreply.github.com>
2024-06-21 23:23:26 +0100

    Add files via upload

--- dwm.c
+++ dwm.c
@@ -722,10 +722,10 @@ drawbar(Monitor *m)
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
 		w = TEXTW(tags[i]);
-		if (!(m->tagset[m->seltags] & 1 << i || occ & 1 <<i)) continue;
+		if (!(m->tagset[m->seltags] & 1 << i || occ & 1 <<i)) continue; /*Only displays tags occupied by a window*/
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		// if (occ & 1 << i)
+		// if (occ & 1 << i) /*Doesn't draw the rectangle to indicate an occupied tag*/
 		//	drw_rect(drw, x + boxs, boxs, boxw, boxw,
 		//		m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 		//		urg & 1 << i);
