b06e56e5077d209b1b65cfe8fd83fbd802655aaa
ProsperousPotato <114923153+ProsperousPotato@users.noreply.github.com>
2024-08-11 12:18:01 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -2,22 +2,22 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Constants */
-#define TERMINAL "st"
+#define TERMINAL "urxvtc"
 #define BROWSER "librewolf-bin"
-#define FILE_EXPLORER "dolphin"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 16;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "inconsolata-mono-nerd:size=12" };
-static const char dmenufont[]       = "inconsolata-mono-nerd:size=12";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#000000";
-static const char col_cyan[]        = "#bbbbbb";
+static const char *fonts[]          = { "cozette:size=12:autohint=true" };
+static const char dmenufont[]       = "cozette:size=12:autohint=true";
+static const char col_gray1[]       = "#000000"; // Colour of the background of the bar
+static const char col_gray2[]       = "#444444"; // Colour of the border of the inactive window
+static const char col_gray3[]       = "#bbbbbb"; // Colour of the foreground of the bar
+static const char col_gray4[]       = "#000000"; // Colour of the wallpaper by default
+static const char col_cyan[]        = "#bbbbbb"; // Colour of the border of the active window
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -25,22 +25,27 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; // These are what is visually displayed in the bar, you can change them to whatever.
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class         instance          title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ NULL,          "steamwebhelper", NULL,           0,         1,          0,           0,        -1 },
+	{ "qBittorrent", NULL,             NULL,           0,         1,          0,          -1,        -1 },
+	{ "St",          NULL,             NULL,           0,         0,          1,           0,        -1 },
+	{ "URxvt",       NULL,             NULL,           0,         1,          1,           0,        -1 },
+	{ "Nsxiv",       NULL,             NULL,           0,         1,          0,           0,        -1 },
+	{ "mpv",         NULL,             NULL,           0,         1,          0,           0,        -1 },
+	{ NULL,          NULL,             "Event Tester", 0,         0,          0,           1,        -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -55,7 +60,7 @@ static const Layout layouts[] = {
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,              KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
@@ -76,7 +81,6 @@ static const char *fasterbrightnessup[] = { "xbacklight", "-inc", "10", NULL };
 static const char *fasterbrightnessdown[] = { "xbacklight", "-dec", "10", NULL };
 static const char *slock[] = { "slock", NULL };
 static const char *browser[] = { BROWSER, NULL };
-static const char *files[] = { FILE_EXPLORER, NULL };
 static const char *steam[] = { "steam", NULL };
 static const char *discord[] = { "discord", NULL };
 
@@ -84,24 +88,22 @@ static const char *discord[] = { "discord", NULL };
 static const Key keys[] = {
 	/* modifier                     key           function        argument */
 	{ MODKEY,                       XK_d,         spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Tab,       spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_space,     spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,         togglebar,      {0} },
 	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,         rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,         rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_i,         incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_u,         incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,         setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,         setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_space,     zoom,           {0} },
-	{ MODKEY|ShiftMask,             XK_space,     view,           {0} },
+	{ Mod1Mask,                     XK_Tab,       zoom,           {0} },
+	{ MODKEY,                       XK_Tab,       view,           {0} },
 	{ MODKEY,                       XK_q,         killclient,     {0} },
 	{ MODKEY,                       XK_t,         setlayout,      {.v = &layouts[0]} }, 
 	{ MODKEY,                       XK_f,         setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,         setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_Return,    setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_Return,    togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_Return,    setlayout,      {0} },
+	{ MODKEY,                       XK_Return,    togglefloating, {0} },
 	{ MODKEY,                       XK_0,         view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,         tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,     focusmon,       {.i = -1 } },
@@ -118,28 +120,33 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                         7)
 	TAGKEYS(                        XK_9,                         8)
 	{ MODKEY|ShiftMask,             XK_q,        quit,            {0} },
-	{ MODKEY,                       XK_Print,                     spawn,  {.v = cmdprintscreen } },
-	{ 0,                            XF86XK_AudioRaiseVolume,      spawn,  {.v = volumeup } },
-	{ 0,                            XF86XK_AudioLowerVolume,      spawn,  {.v = volumedown } },
-	{ MODKEY,                       XK_Up,                        spawn,  {.v = volumeup } },
-	{ MODKEY,                       XK_Down,                      spawn,  {.v = volumedown } },
-	{ MODKEY,                       XK_Right,                     spawn,  {.v = fasterbrightnessup } },
-	{ MODKEY,                       XK_Left,                      spawn,  {.v = fasterbrightnessdown } },
-	{ MODKEY|ControlMask,           XK_Right,                     spawn,  {.v = brightnessup } },
-	{ MODKEY|ControlMask,           XK_Left,                      spawn,  {.v = brightnessdown } },
-	{ 0,                            XF86XK_AudioMute,             spawn,  {.v = volumetoggle } },
-	{ 0,                            XF86XK_MonBrightnessUp,       spawn,  {.v = fasterbrightnessup } },
-	{ 0,                            XF86XK_MonBrightnessDown,     spawn,  {.v = fasterbrightnessdown } },
-	{ 0|ControlMask,                XF86XK_MonBrightnessUp,       spawn,  {.v = brightnessup } },
-	{ 0|ControlMask,                XF86XK_MonBrightnessDown,     spawn,  {.v = brightnessdown } },
-	{ 0,                            XF86XK_AudioMicMute,          spawn,  {.v = mictoggle } },
-	{ MODKEY|ShiftMask,             XK_w,                         spawn,  {.v = browser } },
-	{ MODKEY|ShiftMask,             XK_e,                         spawn,  {.v = files } },
-	{ MODKEY|ShiftMask,             XK_s,                         spawn,  {.v = steam } },
-	{ MODKEY|ShiftMask,             XK_d,                         spawn,  {.v = discord } },
-	{ Mod1Mask|ControlMask,         XK_Delete,                    spawn,  {.v = slock } },
-	{ MODKEY|ShiftMask,             XK_v,                         spawn,  SHCMD("~/.local/bin/./bookmarkthis.sh") },
-	{ MODKEY,                       XK_v,                         spawn,  SHCMD("setxkbmap gb && xdotool type $(grep -v '^#' ~/.local/share/bookmarks/bookmarksfile | dmenu -i -l 50)") },
+  /* Non-default commands i.e. commands added in by me and patches */
+	{ MODKEY|ShiftMask,             XK_j,                         rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,                         rotatestack,    {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_m,                         spawn,          {.v = (const char*[]){ TERMINAL, "-e", "neomutt", NULL } }},
+    { ControlMask|ShiftMask,        XK_Escape,                    spawn,          {.v = (const char*[]){ TERMINAL, "-e", "btop", NULL } }},
+    { MODKEY|ShiftMask,             XK_e,                         spawn,          {.v = (const char*[]){ TERMINAL, "-e", "lfcd", NULL } }},
+	{ MODKEY,                       XK_Print,                     spawn,          {.v = cmdprintscreen } },
+	{ 0,                            XF86XK_AudioRaiseVolume,      spawn,          {.v = volumeup } },
+	{ 0,                            XF86XK_AudioLowerVolume,      spawn,          {.v = volumedown } },
+	{ MODKEY,                       XK_Up,                        spawn,          {.v = volumeup } },
+	{ MODKEY,                       XK_Down,                      spawn,          {.v = volumedown } },
+	{ MODKEY,                       XK_Right,                     spawn,          {.v = fasterbrightnessup } },
+	{ MODKEY,                       XK_Left,                      spawn,          {.v = fasterbrightnessdown } },
+	{ MODKEY|ControlMask,           XK_Right,                     spawn,          {.v = brightnessup } },
+	{ MODKEY|ControlMask,           XK_Left,                      spawn,          {.v = brightnessdown } },
+	{ 0,                            XF86XK_AudioMute,             spawn,          {.v = volumetoggle } },
+	{ 0,                            XF86XK_MonBrightnessUp,       spawn,          {.v = fasterbrightnessup } },
+	{ 0,                            XF86XK_MonBrightnessDown,     spawn,          {.v = fasterbrightnessdown } },
+	{ 0|ControlMask,                XF86XK_MonBrightnessUp,       spawn,          {.v = brightnessup } },
+	{ 0|ControlMask,                XF86XK_MonBrightnessDown,     spawn,          {.v = brightnessdown } },
+	{ 0,                            XF86XK_AudioMicMute,          spawn,          {.v = mictoggle } },
+	{ MODKEY|ShiftMask,             XK_w,                         spawn,          {.v = browser } },
+	{ MODKEY|ShiftMask,             XK_s,                         spawn,          {.v = steam } },
+	{ MODKEY|ShiftMask,             XK_d,                         spawn,          {.v = discord } },
+	{ Mod1Mask|ControlMask,         XK_Delete,                    spawn,          {.v = slock } },
+	{ MODKEY|ShiftMask,             XK_v,                         spawn,          SHCMD("~/.local/bin/./bookmarkthis") },
+	{ MODKEY,                       XK_v,                         spawn,          SHCMD("setxkbmap gb && xdotool type $(grep -v '^#' ~/.local/bin/bookmarksfile | dmenu -i -l 50)") },
 };
 
 /* button definitions */
