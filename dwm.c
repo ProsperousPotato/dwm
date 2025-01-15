66f3592108fa87df39f1d5b53437852a4ce74134
ProsperousPotato <ProsperousPotato@users.noreply.github.com>
2025-01-15 19:40:48 +0000

    update config

--- dwm.c
+++ dwm.c
@@ -1591,6 +1591,16 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	c->oldw = c->w; c->w = wc.width = w;
 	c->oldh = c->h; c->h = wc.height = h;
 	wc.border_width = c->bw;
+    /* Hide border if only 1 client on screen */
+    if (((nexttiled(c->mon->clients) == c && !nexttiled(c->next))
+        || &monocle == c->mon->lt[c->mon->sellt]->arrange)
+        && !c->isfullscreen && !c->isfloating
+        && NULL != c->mon->lt[c->mon->sellt]->arrange) {
+            c->w = wc.width += c->bw * 2;
+            c->h = wc.height += c->bw * 2;
+            wc.border_width = 0;
+    }
+
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
 	configure(c);
 	XSync(dpy, False);
@@ -2069,6 +2079,11 @@ tile(Monitor *m)
 	Client *c;
 
 	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+    if (n == 1)
+        m->gappx = 0;
+    else
+     m->gappx = gappx;
+
 	if (n == 0)
 		return;
 
